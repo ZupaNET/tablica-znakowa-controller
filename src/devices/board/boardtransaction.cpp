81e722a9350ad31012ee71e9dd716fb6a6e6ba17
rikka <rikka@takanashi.moe>
2026-08-14 15:11:58 +0200

    docs(BoardTransaction): add copyright information

--- src/devices/board/boardtransaction.cpp
+++ src/devices/board/boardtransaction.cpp
@@ -1,3 +1,10 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Tablica Znakowa - Kontroler
+ *
+ * Copyright (C) 2026 ŻupaNET Development <devel@zupanet.pl>
+ */
+
 #include "boardtransaction.h"
 #include "boardcommandsession.h"
 #include "settings/appsettings.h"
--- src/devices/board/boardtransaction.h
+++ src/devices/board/boardtransaction.h
@@ -1,3 +1,10 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Tablica Znakowa - Kontroler
+ *
+ * Copyright (C) 2026 ŻupaNET Development <devel@zupanet.pl>
+ */
+
 #ifndef BOARDTRANSACTION_H
 #define BOARDTRANSACTION_H
 
